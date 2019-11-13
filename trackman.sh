4273a899df4c7dccf3bd03c5bc6133d3c4835d31
Michael Zijlstra <mzijlstra@gmail.com>
2019-11-13 16:47:43 -0600

    Updated how to make trackman scroll

--- trackman.sh
+++ trackman.sh
@@ -14,3 +14,15 @@ xinput --set-int-prop \
 
 # make button 9 our 'middle' (aka 2nd) mouse button for pasting
 xmodmap -e "pointer = 1 9 3 4 5 6 7 8 2 10"
+
+#2019-11-13 ubuntu now does this with libinput. 
+#See: https://help.ubuntu.com/community/Logitech_Marblemouse_USB#Horizontal_and_Vertical_Scrolling_Example
+# In my 40-libinput.conf I have:
+#Section "InputClass"
+#       Identifier  "Marble Mouse"
+#       MatchProduct "Logitech USB Trackball"
+#       Driver "libinput"
+#       Option "ScrollMethod" "button"
+#       Option "ScrollButton" "8"
+#       Option "ButtonMapping" "1,9,3,4,5,6,7,8,2"
+#EndSection
